21ecefd3eb61d819c7f1bb2c5ceb0c9e409f75a9
Eric Silva <ericsilvap13@gmail.com>
2025-06-16 14:57:03 -0300

    validação sinais vitais

--- src/app/enfermeiro/sinaisVitais/page.tsx
+++ src/app/enfermeiro/sinaisVitais/page.tsx
@@ -1,14 +1,23 @@
 'use client'
 
-import { replaceOnlyNumbers } from "@/app/cadastroPessoa/page"
-import { useForm } from "react-hook-form"
+import { Controller, useForm } from "react-hook-form"
 
 export default function SinaisVitais() {
     const {
         register,
         handleSubmit,
+        control,
         formState: { errors }
     } = useForm()
+
+    enum pain_level {
+        LIGHT = "Leve",
+        MEDIUM = "Moderada",
+        INTENSE = "Intensa",
+        EXTREME = "Incapacitante"
+    }
+
+    const pain_levels = Object.values(pain_level)
 
     function onSubmit(data: unknown) {
         alert(JSON.stringify(data))
@@ -17,7 +26,7 @@ export default function SinaisVitais() {
     function replaceNumbersNPoint(data: string) {
         return data.replace(/[^\d\.]/, "")
     }
-    
+
     return (
         <div className="mt-[3rem] flex justify-center">
             <div className="flex flex-col gap-10">
@@ -27,58 +36,131 @@ export default function SinaisVitais() {
                         <div className="text-white flex-5">
                             Frequência Cardíaca (bpm):
                         </div>
-                        <input type="text" className={`custom-input flex-5 ${errors.heart_rate && 'outline-2 outline-[rgb(240,101,58)]'}`} 
-                        
-                        {...register("heart_rate", {
-                            required: true,
-                            onChange: (e) => replaceNumbersNPoint(e.currentTarget.value)
-                        })} 
-                        />
+                        <Controller
+                            name="heart_rate"
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) => (
+
+                                <input
+                                    {...field}
+                                    onChange={(e) => {
+                                        const formatted = replaceNumbersNPoint(e.target.value)
+                                        field.onChange(formatted)
+                                    }}
+                                    value={field.value}
+                                    type="text" className={`custom-input flex-5 ${errors.heart_rate && 'outline-2 outline-[rgb(240,101,58)]'}`}
+                                />
+                            )} />
                     </div>
                     <div className="flex items-center">
                         <div className="text-white flex-5">
                             Frequência Respiratória (bpm):
                         </div>
-                        <input type="text" className={`custom-input flex-5 ${errors.respiratory_rate && 'outline-2 outline-[rgb(240,101,58)]'}`} 
-                        {...register("respiratory_rate", {required: true})}
-                        />
+                        <Controller
+                            name="respiratory_rate"
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) => (
+                                <input
+                                    {...field}
+                                    onChange={(e) => {
+                                        const formatted = replaceNumbersNPoint(e.target.value)
+                                        field.onChange(formatted)
+                                    }}
+                                    value={field.value}
+                                    type="text" className={`custom-input flex-5 ${errors.respiratory_rate && 'outline-2 outline-[rgb(240,101,58)]'}`}
+                                />
+                            )} />
                     </div>
                     <div className="flex items-center">
                         <div className="text-white flex-5">
                             Pressão Arterial (mmHg):
                         </div>
-                        <input type="text" 
-                        className={`custom-input flex-5 ${errors.blood_pressure && 'outline-2 outline-[rgb(240,101,58)]'}`} 
-                        {...register("blood_pressure", {required: true})}
-                        />
+                        <Controller
+                            name="blood_pressure"
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) => (
+                                <input
+                                    {...field}
+                                    onChange={(e) => {
+                                        const formatted = replaceNumbersNPoint(e.target.value)
+                                        field.onChange(formatted)
+                                    }}
+                                    value={field.value}
+                                    type="text" className={`custom-input flex-5 ${errors.blood_pressure && 'outline-2 outline-[rgb(240,101,58)]'}`}
+                                />
+                            )} />
                     </div>
                     <div className="flex items-center">
                         <div className="text-white flex-5">
                             Temperatura Corporal (°C):
                         </div>
-                        <input 
-                        type="text" 
-                        className={`custom-input flex-5 ${errors.temperature && 'outline-2 outline-[rgb(240,101,58)]'}`} 
-                        {...register("temperature", {required: true})}
-                        />
+                        <Controller
+                            name="temperature"
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) => (
+                                <input
+                                    {...field}
+                                    onChange={(e) => {
+                                        const formatted = replaceNumbersNPoint(e.target.value)
+                                        field.onChange(formatted)
+                                    }}
+                                    value={field.value}
+                                    type="text" className={`custom-input flex-5 ${errors.temperature && 'outline-2 outline-[rgb(240,101,58)]'}`}
+                                />
+                            )} />
                     </div>
                     <div className="flex items-center">
-                        <div className="text-white flex-5"> 
+                        <div className="text-white flex-5">
                             Saturação O² (%):
                         </div>
-                        <input 
-                        type="text" 
-                        className={`custom-input flex-5 ${errors.oxygen_saturation && 'outline-2 outline-[rgb(240,101,58)]'}`} 
-                        {...register("oxygen_saturation", {required: true})}
-                        />
+                        <Controller
+                            name="oxygen_saturation"
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) => (
+
+                                <input
+                                    {...field}
+                                    onChange={(e) => {
+                                        const formatted = replaceNumbersNPoint(e.target.value)
+                                        field.onChange(formatted)
+                                    }}
+                                    value={field.value}
+                                    type="text" className={`custom-input flex-5 ${errors.oxygen_saturation && 'outline-2 outline-[rgb(240,101,58)]'}`}
+                                />
+                            )} />
+                    </div>
+                    <div className="flex items-center gap-8">
+                        <div className="text-white flex-5">
+                            Nível de Dor:
+                        </div>
+                        <div className="flex justify-between gap-6 flex-5">
+                            {
+                                pain_levels.map((pl, i) => (
+                                    <label className="flex gap-3" key={pl}>
+                                        <input 
+                                        type="radio" 
+                                        value={i} 
+                                        className="scale-200"
+                                        {...register("pain_level", { required: true })}
+                                        />
+                                        <div>{pl}</div>
+                                    </label>
+                                ))
+                            }
+                        </div>
                     </div>
                 </main>
                 <div className="flex justify-evenly">
                     <div>
-                        <button className="bg-[rgb(56,163,165)] p-2 text-white text-2xl  font-bold rounded min-w-[9rem]" onClick={() => handleSubmit(onSubmit)()}>Voltar</button>
+                        <button className="bg-[rgb(56,163,165)] p-2 text-white text-2xl  font-bold rounded min-w-[9rem] cursor-pointer shadow-2xl hover:opacity-[90%] transition duration-150" onClick={() => handleSubmit(onSubmit)()}>Voltar</button>
                     </div>
                     <div>
-                        <button className="bg-[rgb(56,163,165)] p-2 text-white text-2xl font-bold rounded min-w-[9rem]" onClick={() => handleSubmit(onSubmit)()}>Próximo</button>
+                        <button className="bg-[rgb(56,163,165)] p-2 text-white text-2xl font-bold rounded min-w-[9rem] cursor-pointer shadow-2xl hover:opacity-[90%] transition duration-150" onClick={() => handleSubmit(onSubmit)()}>Próximo</button>
                     </div>
                 </div>
             </div>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -12,11 +12,11 @@ export function Header() {
                 <div className='flex gap-2 items-center relative'>
                     <Link href={"/"}>
                         <div className='h-[55px] w-[55px] -translate-y-1 cursor-pointer hover:bg-[rgba(0,0,0,0.2)] transition ease-in-out duration-300 rounded-full flex items-center justify-center'>
-                            <Image src="home.svg" alt='' width={35} height={35} className='text-white'/>
+                            <Image src="/home.svg" alt='' width={35} height={35} className='text-white'/>
                         </div>
                     </Link>
                     <div className='h-[55px] w-[55px] -translate-y-1 cursor-pointer hover:bg-[rgba(0,0,0,0.2)] transition ease-in-out duration-300 rounded-full flex items-center justify-center p-2'>
-                        <Image src={"menu_24_white.svg"} alt='' width={45} height={45} />
+                        <Image src={"/menu_24_white.svg"} alt='' width={45} height={45} />
                     </div>
                 </div>
             </header>
