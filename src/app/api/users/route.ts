bbb5490b237c454a79f3ec163f59f9eff98be5e3
Eric Silva <ericsilvap13@gmail.com>
2025-06-23 15:49:34 -0300

    api usuarios delete

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,4 +1,6 @@
+import { deleteDoctorById } from "@/database/doctorRespository";
 import { deleteNurseById } from "@/database/nurseRepository";
+import { deleteRecepcionistaById } from "@/database/receptionistRepository";
 import { createUser, deleteUserByEmail, getAllUsers, getUserByEmail } from "@/database/userRepository";
 import { NextResponse } from "next/server";
 
@@ -25,49 +27,49 @@ export async function POST(req: Request) {
   }
 }
 
-// export async function DELETE(req: Request) {
-//   try {
-//     const { email } = await req.json();
-//     if (!email) {
-//       return NextResponse.json({ error: "Email é obrigatório" }, { status: 400 });
-//     }
+export async function DELETE(req: Request) {
+  try {
+    const { email } = await req.json();
+    if (!email) {
+      return NextResponse.json({ error: "Email é obrigatório" }, { status: 400 });
+    }
 
-//     // 1. Busca usuário
-//     const user = getUserByEmail(email);
-//     if (!user) {
-//       return NextResponse.json({ error: "Usuário não encontrado" }, { status: 404 });
-//     }
+    // 1. Busca usuário
+    const user = getUserByEmail(email);
+    if (!user) {
+      return NextResponse.json({ error: "Usuário não encontrado" }, { status: 404 });
+    }
 
-//     const { papel, referencia_id } = user;
+    const { papel, referencia_id } = user;
 
-//     // 2. Deleta funcionário conforme papel
-//     let deletedEmployeeCount = 0;
-//     switch (papel) {
-//       case "medico":
-//         deletedEmployeeCount = deleteMedicoById(referencia_id);
-//         break;
-//       case "enfermeira":
-//         deletedEmployeeCount = deleteNurseById(referencia_id);
-//         break;
-//       case "recepcionista":
-//         deletedEmployeeCount = deleteRecepcionistaById(referencia_id);
-//         break;
-//       default:
-//         return NextResponse.json({ error: "Papel inválido" }, { status: 400 });
-//     }
+    // 2. Deleta funcionário conforme papel
+    let deletedEmployeeCount = 0;
+    switch (papel) {
+      case "medico":
+        deletedEmployeeCount = deleteDoctorById(referencia_id);
+        break;
+      case "enfermeira":
+        deletedEmployeeCount = deleteNurseById(referencia_id);
+        break;
+      case "recepcionista":
+        deletedEmployeeCount = deleteRecepcionistaById(referencia_id);
+        break;
+      default:
+        return NextResponse.json({ error: "Papel inválido" }, { status: 400 });
+    }
 
-//     if (deletedEmployeeCount === 0) {
-//       return NextResponse.json({ error: "Funcionário não encontrado para deleção" }, { status: 404 });
-//     }
+    if (deletedEmployeeCount === 0) {
+      return NextResponse.json({ error: "Funcionário não encontrado para deleção" }, { status: 404 });
+    }
 
-//     // 3. Deleta usuário
-//     const deletedUserCount = deleteUserByEmail(email);
-//     if (deletedUserCount === 0) {
-//       return NextResponse.json({ error: "Falha ao deletar usuário" }, { status: 500 });
-//     }
+    // 3. Deleta usuário
+    const deletedUserCount = deleteUserByEmail(email);
+    if (deletedUserCount === 0) {
+      return NextResponse.json({ error: "Falha ao deletar usuário" }, { status: 500 });
+    }
 
-//     return NextResponse.json({ message: "Usuário e funcionário deletados com sucesso" });
-//   } catch (error: any) {
-//     return NextResponse.json({ error: error.message }, { status: 500 });
-//   }
-// }
+    return NextResponse.json({ message: "Usuário e funcionário deletados com sucesso" });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
--- src/database/userRepository.ts
+++ src/database/userRepository.ts
@@ -1,5 +1,13 @@
 import { db } from "."
 
+interface User {
+  usuario_id: number;
+  email: string;
+  senha: string;
+  papel: 'medico' | 'enfermeira' | 'recepcionista' | 'admin';
+  referencia_id: number;
+}
+
 export function createUser(email: string, password: string, role: string, reference_id: number) {
   const stmt = db.prepare('INSERT INTO Usuario (email, senha, papel, referencia_id) VALUES (?, ?, ?, ?)')
   const info = stmt.run(email, password, role, reference_id)
@@ -14,12 +22,12 @@ export function getAllUsers() {
 
 export function getUserByEmail(email: string) {
   const stmt = db.prepare('SELECT * FROM Usuario WHERE email = @email')
-  return stmt.get({ email })
+  return stmt.get({ email }) as User || undefined 
 }
 
 
 export function deleteUserByEmail(email: string) {
-  const stmt = db.prepare("DELETE FROM Usuario WHERE email = ?");
-  const info = stmt.run(email);
-  return info.changes; // número de linhas deletadas
+  const stmt = db.prepare("DELETE FROM Usuario WHERE email = ?")
+  const info = stmt.run(email) 
+  return info.changes // número de linhas deletadas
 }
