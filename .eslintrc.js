cf9436aa9ba2f93e13dd1b9c303ec5b14be6b890
Eric Silva <ericsilvap13@gmail.com>
2025-06-23 22:07:33 -0300

    prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "next/core-web-vitals"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": "warn"
+  },
+}
--- src/database/userRepository.ts
+++ src/database/userRepository.ts
@@ -1,4 +1,4 @@
-import { db } from "./index"
+import { db } from './index'
 
 interface User {
   usuario_id: number;
