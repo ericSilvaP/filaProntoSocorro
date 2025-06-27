6570c1e3e5cb6ecbdf01d0019fe6856f19ffd291
Eric Silva <ericsilvap13@gmail.com>
2025-06-27 01:39:26 -0300

    admin responsivo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -70,23 +70,23 @@ export function Header() {
         </div>
       </div>
       
-      <header className="flex justify-between px-5 py-3 bg-[rgb(56,163,165)] shadow-xl">
+      <header className="flex justify-between px-5 py-1.5 bg-[rgb(56,163,165)] shadow-xl">
         <Image
           src="/systemLogo.svg"
           alt=""
-          width={50}
-          height={50}
+          width={35}
+          height={35}
           className="text-white select-none "
         />
 
         <div className="flex gap-2 items-center relative">
           <Link href={'/filaExibicao'}>
-            <div className="h-[55px] w-[55px] -translate-y-1 cursor-pointer hover:bg-[rgba(0,0,0,0.2)] transition ease-in-out duration-300 rounded-full flex items-center justify-center">
-              <Image src="/home.svg" alt="" width={35} height={35} className="text-white" />
+            <div className="h-[45px] w-[45px] -translate-y-1 cursor-pointer hover:bg-[rgba(0,0,0,0.2)] transition ease-in-out duration-300 rounded-full flex items-center justify-center">
+              <Image src="/home.svg" alt="" width={25} height={25} className="text-white" />
             </div>
-          </Link>
-          <div className="h-[55px] w-[55px] -translate-y-1 cursor-pointer hover:bg-[rgba(0,0,0,0.2)] transition ease-in-out duration-300 rounded-full flex items-center justify-center p-2" onClick={toggleMenu}>
-            <Image src={'/menu_24_white.svg'} alt="" width={45} height={45} />
+          </Link> 
+          <div className="h-[45px] w-[45px] -translate-y-1 cursor-pointer hover:bg-[rgba(0,0,0,0.2)] transition ease-in-out duration-300 rounded-full flex items-center justify-center p-2" onClick={toggleMenu}>
+            <Image src={'/menu_24_white.svg'} alt="" width={35} height={35} />
           </div>
         </div>
       </header>
