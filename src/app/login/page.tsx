6d6b6757e377e76211a09d8aa8ef5aab5e2e4436
Eric Silva <ericsilvap13@gmail.com>
2025-06-27 01:29:07 -0300

    login responsivo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -55,27 +55,27 @@ export default function Home() {
 
   return (
     <div className="flex justify-center items-center w-full h-screen px-4 font-[family-name:var(--font-gabarito)]">
-      <div className="bg-[rgb(56,163,165)] w-full max-w-xs md:max-w-[500px] flex flex-col p-6 md:p-10 gap-10 rounded-2xl items-center shadow-2xl">
+      <div className="bg-[rgb(56,163,165)] max-w-[280px] md:max-w-[320px] md:max-h-[400px] flex flex-col p-3 md:p-8 gap-10 rounded-2xl items-center shadow-2xl -translate-y-5">
         
         <div className="flex flex-col items-center">
           <Image
             src="/systemLogoShadow.svg"
             alt=""
-            height={100}
-            width={100}
+            height={80}
+            width={80}
             className="select-none"
           />
-          <label className="text-white font-bold text-3xl md:text-4xl mt-2">MedLink</label>
+          <label className="text-white font-bold text-xl md:text-2xl mt-2">MedLink</label>
         </div>
 
         <div className="flex flex-col items-center gap-5 w-full">
           <label className={`bg-white flex items-center p-3 gap-4 w-full rounded ${errors.email ? 'outline-2 outline-[rgb(240,101,58)]' : ''}`}>
             <Image src="/mail_32_black.svg" alt="" height={24} width={24} />
-            <span className="text-base md:text-lg">Email</span>
+            <span className="text-[12px] md:text-[15px">Email</span>
             <input
               autoComplete='off'
               type="text"
-              className="flex-1 text-base md:text-lg focus:outline-none"
+              className="flex-1 text-[10px] md:text-[12px] focus:outline-none"
               {...register('email', {
                 required: true,
                 validate: (value) => isEmail(value),
@@ -85,11 +85,11 @@ export default function Home() {
 
           <label className={`bg-white flex items-center p-3 gap-4 w-full rounded ${errors.password ? 'outline-2 outline-[rgb(240,101,58)]' : ''}`}>
             <Image src="/lock_32_black.svg" alt="" height={24} width={24} />
-            <span className="text-base md:text-lg">Senha</span>
+            <span className="text-[10px] md:text-[12px]">Senha</span>
             <input
               autoComplete='off'
               type={showPassword ? 'password' : 'text'}
-              className="flex-1 text-base md:text-lg focus:outline-none"
+              className="flex-1 text-[10px] md:text-[12px] focus:outline-none"
               {...register('password', { required: true })}
             />
             <button
@@ -109,7 +109,7 @@ export default function Home() {
 
         <div className="w-full">
           <button
-            className="bg-[rgb(128,237,153)] shadow-md py-3 px-6 font-semibold text-lg md:text-2xl rounded hover:brightness-110 transition-all w-full"
+            className="bg-[rgb(128,237,153)] shadow-md py-1.5 px-3 font-semibold text- md:text-[15px] rounded hover:brightness-110 transition-all w-full"
             onClick={() => handleSubmit(onSubmit)()}
           >
             Entrar
